715828f8dc0153d8ecba31e83848b338e2af53b1
Dorina Diem <diem@arvatis.com>
2022-03-11 17:06:01 +0100

    added difficulty level random

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ a wordl clone in python for the command line
 ## ToDos
 * count letters wordle-like !
 * show full guess-graph at the end
-* random difficulty
 * difficulty wordle (5)
 * new word-file 
 * give ability to choose a word-file
--- TheLevel.py
+++ TheLevel.py
@@ -6,10 +6,12 @@ class TheLevel:
         self.level_medium = (5, 7)
         self.level_hard = (8, 10)
         self.level_extra_hard = (11, 999)
+        self.level_random = (2, 999)
 
     def get_level_from_user(self):
         print("Choose your level")
-        print("1 = easy (2-4 letters), 2 = medium (5-7 letters), 3 = hard (8-10 letters), 4 = extra hard (11+ letters)")
+        print("1 = easy (2-4 letters), 2 = medium (5-7 letters), 3 = hard (8-10 letters),")
+        print("4 = extra hard (11+ letters), 5 = random (2+ letters)")
         user_level = int(input("Enter Level: "))
 
         return user_level
@@ -23,3 +25,5 @@ class TheLevel:
             return self.level_hard
         if self.level == 4:
             return self.level_extra_hard
+        if self.level == 5:
+            return self.level_random
